19bd56a02adcc526fd53b185323e4dec4ce500da
Siddharth966 <146622482+Siddharth966@users.noreply.github.com>
2026-05-03 22:53:44 +0530

    uytftg

--- server/db.js
+++ server/db.js
@@ -3,7 +3,7 @@ import mongoose from "mongoose";
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.DB_NAME);
+    await mongoose.connect(process.env.MONGODB_URI);
     console.log("✅ MongoDB connected");
   } catch (error) {
     console.error("❌ DB connection error:", error.message);
